9dc09f7e06cc34d1f4ffed1cb4abac6aa026e827
Tyler Cadena <tylercadena@alum.calarts.edu>
2020-03-03 19:31:36 -0600

    added delegate stuff

--- 1_csharp/MediaWorld/MediaWorld.Client/Program.cs
+++ 1_csharp/MediaWorld/MediaWorld.Client/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using MediaWorld.Domain.Models; 
+using System.Linq;
+using MediaWorld.Domain.Models;
 using MediaWorld.Domain.Singletons; 
 using MediaWorld.Storage.Adapters; 
 using MediaWorld.Storage.Repositories;
@@ -12,10 +13,24 @@ namespace MediaWorld.Client {
         }
         private static void PlayBook() {
             var b = new Book();
-            b.Read(HowToRead.Upper);
-            b.ReadAction((string s) => { Console.WriteLine(s.ToLowerInvariant()); });
-            b.ReadFunction((string s) => { Console.WriteLine(s.ToUpperInvariant()); return string.Empty; });
-            b.ReadDelegate((string s) => { Console.WriteLine(s.ToLowerInvariant()); });
+            // b.Read(HowToRead.Upper);
+            // b.ReadAction((string s) => { Console.WriteLine(s.ToLowerInvariant()); });
+            // b.ReadFunction((string s) => { Console.WriteLine(s.ToUpperInvariant()); return string.Empty; });
+            // b.ReadDelegate((string s) => { Console.WriteLine(s.ToLowerInvariant()); });
+            // b.ReadDelegate(UseCasingDelegate);
+            // b.ReadDelegate2(UseCasingDelegate2);
+            // event
+            b.Open();
+            b.BookEvent += UseEvent;
+        }
+        public static void UseEvent(string s) {
+            Console.WriteLine(s);
+        }
+        private static void UseCasingDelegate(string s) {
+            Console.WriteLine(s);
+        }
+        private static string UseCasingDelegate2() {
+            return "delegate rules!!!";
         }
         private static void PlayAudio() {
             var ap = AudioPlayer.Instance;
@@ -26,12 +41,12 @@ namespace MediaWorld.Client {
                 foreach (var item in ac.List()) {
                     ap.Play(item);
                 }
-            } catch (NullReferenceException) {
-                Console.WriteLine("NO Playlist for you");
-            } catch (IndexOutOfRangeException) {
-                Console.WriteLine("NO song for you");
-            } catch (Exception err) {
-                throw new Exception("error", err);
+            } catch (NullReferenceException ex) {
+                Console.WriteLine(ex.Message);
+            } catch (IndexOutOfRangeException ex) {
+                Console.WriteLine(ex.Message);
+            } catch (Exception ex) {
+                throw new Exception("Error!", ex);
             } finally {
                 GC.Collect();
             }
--- 1_csharp/MediaWorld/MediaWorld.Domain/Models/Book.cs
+++ 1_csharp/MediaWorld/MediaWorld.Domain/Models/Book.cs
@@ -1,9 +1,12 @@
 using System;
+using System.Threading;
 using MediaWorld.Domain.Abstracts;
 
 namespace MediaWorld.Domain.Models {
 	public class Book : AAudio {
 		public delegate void CasingDelegate(string s);
+		public delegate string CasingDelegate2();
+		public event CasingDelegate BookEvent;
 		public void Read(HowToRead r) {
 			var text = "Read The Book";
 			switch (r) {
@@ -26,6 +29,19 @@ namespace MediaWorld.Domain.Models {
 		public void ReadDelegate(CasingDelegate d) {
 			var text = "read from delegate";
 			d(text);
+		}
+		public void ReadDelegate2(CasingDelegate2 d) {
+			Console.WriteLine(d());
+		}
+		public void Open() {
+			if (BookEvent != null) {
+				int count = 0;
+				while (count < 10) {
+					this.BookEvent("someone opened a book!");
+					Thread.Sleep(1000);
+					count++;
+				}
+			}
 		}
 	}
 	public enum HowToRead {
