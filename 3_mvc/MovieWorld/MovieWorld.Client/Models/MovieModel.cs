7d6c4d6403a4138cc39aa2231d8f537e6051b911
Tyler Cadena <tylercadena@alum.calarts.edu>
2020-03-21 15:52:21 -0500

    added stuff

--- 3_mvc/MovieWorld/MovieWorld.Client/Models/MovieModel.cs
+++ 3_mvc/MovieWorld/MovieWorld.Client/Models/MovieModel.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MovieWorld.Client.Models;
 
 namespace MovieWorld.Client.Models {
 	public class MovieModel {
+		[Required]
+		[StringLength(150)]
 		public string Title { get; set; }
+		[Required(ErrorMessage = "Please fill out a genre")]
+		[DataType(DataType.Text)]
 		public string Genre { get; set; }
 	}
 }
